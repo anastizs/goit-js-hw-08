012de6a155df76429901ade70a57f36dbb31b483
Anastasiia <dessertkiev@gmail.com>
2023-02-12 21:56:50 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,11 +2,7 @@
 import { galleryItems } from './gallery-items';
 // Описаний в документації
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Change code below this line
-
-console.log(galleryItems);
 
 const imgContainer = document.querySelector('.gallery');
 
