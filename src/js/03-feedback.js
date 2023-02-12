a7c79899e214fe8f5d6103ec5c8369c846274d4c
Anastasiia <dessertkiev@gmail.com>
2023-02-12 21:47:18 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+};
+populateForm();
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+const formData = {};
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateForm() {
+  if (!localStorage.getItem(STORAGE_KEY)) {
+    return;
+  }
+
+  try {
+    const savedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    refs.email.value = savedForm.email;
+    refs.message.value = savedForm.message;
+  } catch (error) {
+    alert('Не виходить отримати дані.');
+  }
+}
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
